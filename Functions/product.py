26eea314641a9c51650247053b9b820230614013
baviamohan <102658218+baviamohan@users.noreply.github.com>
2022-03-30 20:37:45 +0530

    Create product.py

---
+++ Functions/product.py
@@ -0,0 +1,17 @@
+def product_or_sum(a,b):
+  sum=0
+  rem=0
+  product=a*b
+  if(product > 500):
+      print("product of",a,"and",b,"is greater than 500")
+      print("product is:",product)
+      while(product>0):
+        rem=product%10
+        sum=sum+rem
+        product=product//10
+      print("sum of their product is",sum)
+  else:
+      print("product is:",product)
+n1=int(input("enter n1 value:"))
+n2=int(input("enter n2 value:"))
+product_or_sum(n1,n2)
