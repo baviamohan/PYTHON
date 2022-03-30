2edd0e7b260f2db3a5e1a97bab9c20d98232f16f
baviamohan <102658218+baviamohan@users.noreply.github.com>
2022-03-30 21:04:42 +0530

    Update posneg.py

--- Functions/posneg.py
+++ Functions/posneg.py
@@ -1,12 +1,16 @@
-list=[-21,46,-34,15,-10,24]
-def numbers(num):
-  pos=0
-  neg=0
-  for j in num:
-    if(j>0):
-      pos=pos+1
+NumList = []
+Positive_count = 0
+Negative_count = 0
+Number = int(input("Enter the Total Number of List Elements: "))
+for i in range(1, Number + 1):
+    value = int(input("Enter the Value of %d Element: " %i))
+    NumList.append(value)
+print("\nList Entered By the User: ",NumList)
+for j in range(Number):
+    if(NumList[j] >= 0):
+        Positive_count = Positive_count + 1
     else:
-      neg=neg+1
-  print("Number of positive numbers:",pos)
-  print("Number of negative numbers:",neg)
-numbers(list)
+        Negative_count = Negative_count + 1
+print("\nTotal Number of Positive Numbers in this List =  ", Positive_count)
+print("Total Number of Negative Numbers in this List = ", Negative_count)
+
